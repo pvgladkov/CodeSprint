8ebaed826013bd9b207c0a7e01545af7f5c20073
pgladkov <gladkov@idealprice.ru>
2012-05-12 19:46:46 +0400

    ants

--- Ants.cpp
+++ Ants.cpp
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<vector>
 using namespace std ;
 #define MAXN 100002
 
@@ -48,13 +49,102 @@ int* getFirtsAntDirection( int x1, int x2 ){
 	return R;
 }
 
+/**
+ * Вычисление время полной встречи для заданной конфигурации
+ * @param Directions конфигурация (1,0,0,1)
+ * @param Positions координаты
+ * @return 
+ */
+float getTime( std::vector< int > Directions, int Positions[], int Count ){
+	
+	int Po[ MAXN ];
+	int Protiv[ MAXN ];
+	int po, protiv;
+	float totalTime;
+	
+	cout << "Ants " << Count << " ";
+	
+	// считаем время до встречи каждого идущего по часовой стрелки с каждым 
+	// идущим против часовой
+	for( int i = 0; i < Count; i++ ){
+		po = 0;
+		protiv = 0;
+		
+		// разделим по направлениям
+		if( Directions[i] == 0 ){
+			Po[po] = Positions[i];
+			cout << Positions[i] << " ";
+			po++;
+		} else {
+			Protiv[protiv] = Positions[i];
+			cout << Positions[i] << " ";
+			protiv++;
+		}
+	}
+	
+	cout << "Po " << po << " protiv " << protiv << " ";
+	
+	totalTime = 0;
+	for( int i = 0; i < po; i++ ){
+		for( int j = 0; j < protiv; j++ ){
+			if( Po[i] > Protiv[j] ){
+				totalTime = totalTime + ( S - modRazn( Po[i], Protiv[j] ) ) / (2 * V);
+			} else {
+				totalTime = totalTime + modRazn( Po[i], Protiv[j] ) / (2 * V);
+			}
+		}
+	}
+	cout << totalTime << " " ;
+	
+	return totalTime;
+	
+}
+
+int getMeetsCount(){
+	
+}
+
 int getFirstMeetTime( int x1, int x2){
 	
 	
 }
 
-int solve( int Count, int Positions[] ){
+float solve( int Count, int Positions[] ){
 	
+	// конфигурация
+	//int Directions[ MAXN ];
+	int i;
+	std::vector< int > Directions;
+	std::vector< int > tmp;
+	
+	float Times[1000][3];
+	
+	Times[0][0] = S/V;
+	Times[0][1] = S/V;
+	Times[0][3] = S/V;
+	
+	//
+	Directions.push_back(1);
+	for( i = 1; i < Count; i++ ){
+		tmp = Directions;
+		Directions = vector<int>();
+		tmp.push_back(0);
+		cout << tmp[0] << "  " << tmp[1]<< " ";
+		Times[i][0] = Times[i-1][3] + getTime( tmp, Positions, i+1 );
+		tmp = Directions;
+		Directions = vector<int>();
+		tmp.push_back(1);
+		Times[i][1] = Times[i-1][3] + getTime( tmp, Positions, i+1 );
+		if( Times[i][0] > Times[i][1] ){
+			Directions.push_back(1);
+			Times[i][3] = Times[i][1];
+		} else {
+			Directions.push_back(0);
+			Times[i][3] = Times[i][0];
+		}
+		
+	}
+	return Times[i][3];
 }
 
 int main(){
@@ -69,9 +159,9 @@ int main(){
 		scanf( "%d", &Positions[i] );
 		i++;
 	}
-	R = getFirtsAntDirection( Positions[0], Positions[1] );
-	cout << R[0] << " " << R[1] << "\n";
-	cout << floor((T - R[1]) / ( S / (2 * V) )) + 1;
-	//cout << solve( Count, Positions );
+	//R = getFirtsAntDirection( Positions[0], Positions[1] );
+	//cout << R[0] << " " << R[1] << "\n";
+	//cout << floor((T - R[1]) / ( S / (2 * V) )) + 1;
+	cout << solve( i, Positions );
 	return 0;
 }
