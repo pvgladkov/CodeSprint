f1dc4d0ebba834f6655341973916b1dacfcb06f6
webgeist <webgeist.xor@gmail.com>
2012-05-13 15:31:08 +0400

    почти работает

--- Ants.cpp
+++ Ants.cpp
@@ -62,27 +62,29 @@ float getTime( std::vector< int > Directions, int Positions[], int Count ){
 	int po, protiv;
 	float totalTime;
 	
-	cout << "Ants " << Count << " ";
+	//cout << "Ants " << Count << " \n";
+	
 	
 	// считаем время до встречи каждого идущего по часовой стрелки с каждым 
 	// идущим против часовой
+	po = 0;
+	protiv = 0;
 	for( int i = 0; i < Count; i++ ){
-		po = 0;
-		protiv = 0;
+		//cout << "Ant" << i << " Dir-" <<Directions[i] << " pos-" << Positions[i] << "\n";  
 		
 		// разделим по направлениям
 		if( Directions[i] == 0 ){
 			Po[po] = Positions[i];
-			cout << Positions[i] << " ";
+			//cout << Positions[i] << " ";
 			po++;
 		} else {
 			Protiv[protiv] = Positions[i];
-			cout << Positions[i] << " ";
+			//cout << Positions[i] << " ";
 			protiv++;
 		}
 	}
 	
-	cout << "Po " << po << " protiv " << protiv << " ";
+	//cout << "Po " << po << " protiv " << protiv << " ";
 	
 	totalTime = 0;
 	for( int i = 0; i < po; i++ ){
@@ -94,14 +96,61 @@ float getTime( std::vector< int > Directions, int Positions[], int Count ){
 			}
 		}
 	}
-	cout << totalTime << " " ;
+	//cout << "Total time " << totalTime << " \n" ;
 	
 	return totalTime;
 	
 }
 
-int getMeetsCount(){
+/**
+ * Посчитать кол-во встреч для заданной конфигурации
+ * @param Directions направления
+ * @param Positions координаты
+ * @param Count общее кол-во
+ * @return 
+ */
+int getMeetsCount( std::vector< int > Directions, int Positions[], int Count ){
 	
+	// разобьем муравьев по направления и попарно вычислим кол-во встреч 
+	int Po[ MAXN ];
+	int Protiv[ MAXN ];
+	int po, protiv;
+	int totalCount;
+	float Time; // время первой встречи пары
+	
+	po = 0;
+	protiv = 0;
+	for( int i = 0; i < Count; i++ ){
+		//cout << "Ant" << i << " Dir-" <<Directions[i] << " pos-" << Positions[i] << "\n";  
+		
+		// разделим по направлениям
+		if( Directions[i] == 0 ){
+			Po[po] = Positions[i];
+			//cout << Positions[i] << " ";
+			po++;
+		} else {
+			Protiv[protiv] = Positions[i];
+			//cout << Positions[i] << " ";
+			protiv++;
+		}
+	}
+	
+	totalCount = 0;
+	for( int i = 0; i < po; i++ ){
+		for( int j = 0; j < protiv; j++ ){
+			// найдем время их встречи
+			if( Po[i] > Protiv[j] ){
+				Time = ( S - modRazn( Po[i], Protiv[j] ) ) / (2 * V);
+				
+			} else {
+				Time = modRazn( Po[i], Protiv[j] ) / (2 * V);
+			}
+			// теперь посчитаем 
+			totalCount = totalCount + ( floor((T - Time) / ( S / (2 * V) )) + 1 )*2;
+		}
+	}
+	
+	return totalCount;
 }
 
 int getFirstMeetTime( int x1, int x2){
@@ -113,15 +162,15 @@ float solve( int Count, int Positions[] ){
 	
 	// конфигурация
 	//int Directions[ MAXN ];
-	int i;
+	int i, Return;
 	std::vector< int > Directions;
 	std::vector< int > tmp;
 	
 	float Times[1000][3];
 	
-	Times[0][0] = S/V;
-	Times[0][1] = S/V;
-	Times[0][3] = S/V;
+	Times[0][0] = 0;
+	Times[0][1] = 0;
+	Times[0][3] = 0;
 	
 	//
 	Directions.push_back(1);
@@ -129,11 +178,12 @@ float solve( int Count, int Positions[] ){
 		tmp = Directions;
 		Directions = vector<int>();
 		tmp.push_back(0);
-		cout << tmp[0] << "  " << tmp[1]<< " ";
+		//cout << tmp[0] << "  " << tmp[1]<< " ";
+		// [[] 0]
 		Times[i][0] = Times[i-1][3] + getTime( tmp, Positions, i+1 );
-		tmp = Directions;
-		Directions = vector<int>();
+		tmp.pop_back();
 		tmp.push_back(1);
+		// [[] 1]
 		Times[i][1] = Times[i-1][3] + getTime( tmp, Positions, i+1 );
 		if( Times[i][0] > Times[i][1] ){
 			Directions.push_back(1);
@@ -144,7 +194,12 @@ float solve( int Count, int Positions[] ){
 		}
 		
 	}
-	return Times[i][3];
+	
+	tmp = Directions;
+	Directions = vector<int>();
+	Return = getMeetsCount( tmp, Positions, Count );
+	
+	return Return;
 }
 
 int main(){
@@ -155,10 +210,12 @@ int main(){
 	int* R;
 	
 	cin >> Count;
+	
 	while( Count-- ){
 		scanf( "%d", &Positions[i] );
 		i++;
 	}
+	
 	//R = getFirtsAntDirection( Positions[0], Positions[1] );
 	//cout << R[0] << " " << R[1] << "\n";
 	//cout << floor((T - R[1]) / ( S / (2 * V) )) + 1;
